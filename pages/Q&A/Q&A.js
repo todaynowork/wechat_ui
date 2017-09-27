fd411acc7cfd71c6bf8cfacf18539ea925aed299
mumbb <mumbb@cn.ibm.com>
2017-09-27 15:22:39 +0800

    question2

--- pages/Q&A/Q&A.js
+++ pages/Q&A/Q&A.js
@@ -1,116 +1,35 @@
-// pages/Q&A/Q&A.js
 var app = getApp()
 Page({
-  /**
-   * 页面的初始数据
-   */
   data: {
-    questionContent:'为什么老板总让我买果汁？',
-    answerCounts:'3',
-    date:'2017-09-10',
-    userInfo: {},
-    primarySize: 'default',
-    disabled: false,
-    plain: false,
-    loading: false,
-    hidden:true,
-    value:"",
+    QA_user_name:'李文林',
+    QA_body:'如何保证N个线程可以访问N个资源的同时又不导致死锁？',
+    QA_hui_num:'3',
+    QA_date:'2017/09/01',
+    answer1: '多线程产生死锁需要四个条件，分别是互斥性，保持和请求, 不可剥夺性还有要形成闭环，z和四个条件缺一不可，只要破坏了其中一个条件就可以破坏死锁, 其中最简单的方法就是线程以同样的顺序加锁和释放锁，也就是破坏了第四个条件。',
+    answer2: '',
+    form:'form',
+    answers:[
+      [{
+        ans_user_name: "李佳",
+        ans_like_num: '15',
+        ans_dislike_num: '0',
+        ans_body: "多线程产生死锁需要四个条件，分别是互斥性，保持和请求, 不可剥夺性还有要形成闭环，z和四个条件缺一不可，只要破坏了其中一个条件就可以破坏死锁, 其中最简单的方法就是线程以同样的顺序加锁和释放锁，也就是破坏了第四个条件。"
+        ,huifunum:'2',
+        best: true
+      }],
+      [{
+      ans_user_name: "李江浩",
+      ans_like_num: '3',
+      ans_dislike_num: '0',
+      ans_body: "指定线程获取锁的顺序，并强制线程你找指定的方式获取和释放锁。当所有线程都按照顺序获取和释放锁时就不会发生死锁。",
+      huifunum: '2',
+      best: false
+      }]
+    ]
   },
-
-  /**
-   * button监听事件
-   */
-  clickButton: function() {
-    this.setData({
-      hidden:false
-    })
-  },
-
-  answerInput:function(e){
-    this.setData({
-      value:e.detail.value,
-    })
-  },
-  /**
-   * 提交 监听事件
-   */
-  confirm: function () {
+  toanswer: function () {
     wx.navigateTo({
-      url: '../Q&A/Q&A',
+      url: '../answer/answer'
     })
-   
-    
-  },   
-  /**
-   * cancel 监听事件
-   */
-  cancel: function () {
-    wx.navigateTo({
-      url: '../Q&A/Q&A',
-    })
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function (userInfo) {
-      //更新数据
-      that.setData({
-        userInfo: userInfo
-      })
-    })
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
-})
+    }
+});
