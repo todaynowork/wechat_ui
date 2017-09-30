365ddb89500b6300a3ccd51250d067885b60974c
ZHI GUO DU <duzhiguo@cn.ibm.com>
2017-09-30 12:53:23 +0800

    add wxRequest for common use

--- app.js
+++ app.js
@@ -5,6 +5,33 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+    var that = this;
+    wx.login({
+      success: function (res) {
+        if (res.code) {
+          //发起网络请求
+          let openIdReqConf = {
+            url: '/loginPost',
+            data: {
+              code: res.code
+            },
+            success: function (res) {
+              
+              if (res.data.errcode){
+                console.log(res.data);
+                console.log("did not get openid");
+              }else{
+                that.globalData.token = res.data.sessionid;
+              }
+            },
+            method: 'POST'
+          };
+          that.wxRequest("/loginPost", openIdReqConf);
+        } else {
+          console.log('获取用户登录态失败！' + res.errMsg)
+        }
+      }
+    });
   },
 
   getUserInfo: function(cb) {
@@ -14,7 +41,7 @@ App({
     } else {
       //调用登录接口
       wx.getUserInfo({
-        withCredentials: false,
+        withCredentials: true,
         success: function(res) {
           that.globalData.userInfo = res.userInfo
           typeof cb == "function" && cb(that.globalData.userInfo)
@@ -22,9 +49,20 @@ App({
       })
     }
   },
+  wxRequest: function(apiPath,requestConf){
+    let url = this.globalData.apiContextUrl + apiPath;
+    requestConf.url = url;
+    if (this.globalData.token){
+      requestConf.header = requestConf.header || {};
+      requestConf.header['x-token'] = this.globalData.token;
+    }
+    wx.request(requestConf);
+  },
 
   globalData: {
-    userInfo: null
+    userInfo: null,
+    apiContextUrl: 'https://todaynowork.group/wechat-prod-1.0',
+    token : 'null'
   }
 })
 
