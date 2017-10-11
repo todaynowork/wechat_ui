a6713025d3e15121564d811d3637c49345367276
EricWu0119 <bbuwu@cn.ibm.com>
2017-10-11 16:01:37 +0800

    update for tabbar fix and some img fix work

--- pages/History/History.js
+++ pages/History/History.js
@@ -5,13 +5,13 @@ Page({
     _api: {},
     navbar: ['看过的问题', '看过的知识'],
     currentNavbar: 0,
-    themes: [],
+    themes: [{name:'小刘'},{name:'小李'},{name:'小王'}],
     list: [],
     hot_last_id: 0,
     latest_list: [],
     latest_last_id: 0,
-    Knowledge: [{ name: '问题一', content: '由各种物质组成的巨型球状天体，叫做星球。有一定的形状，有自己的运行轨道。' },                  { name: '问题二', content: '由各种物质组成的巨型球状天体，叫做星球。有一定的形状，有自己的运行轨道。'},
-                { name: '问题三', content: '由各种物质组成的巨型球状天体，叫做星球。有一定的形状，有自己的运行轨道。' }]
+    Knowledge: [{ name: '问题一', content: '由各种物质组成的巨型球状天体，叫做星球。有一定的形状，有自己的运行轨道。' }, { name: '问题二', content: '由各种物质组成的巨型球状天体，叫做星球。有一定的形状，有自己的运行轨道。' },
+    { name: '问题三', content: '由各种物质组成的巨型球状天体，叫做星球。有一定的形状，有自己的运行轨道。' }]
   },
   swichNav: function (e) {
     this.setData({
@@ -53,27 +53,28 @@ Page({
 
 
   // },
-  onLoad: function () {
-    console.log('themes.onLoad');
-    var that = this;
-    // 加载数据
-    wx.request({
-      url: 'http://news-at.zhihu.com/api/4/themes',
-      header: { "Content-Type": "application/json" },
-      success: function (res) {
-        var data = res.data;
-        console.log('data = ' + JSON.stringify(data));
-        var temp = [];
-        // temp.push({ 'name': '今日热闻', 'thumbnail': null, 'description': '今日热闻' })
-        for (var i in data.others) {
-          temp.push(data.others[i]);
-        }
-        that.setData({ themes: temp });
-      },
-      fail: function (error) {
-        console.log(error)
-      }
-    });
-  },
+  // onLoad: function () {
+  //   console.log('themes.onLoad');
+  //   var that = this;
+  //   // 加载数据
+  //   wx.request({
+  //     url: 'http://news-at.zhihu.com/api/4/themes',
+  //     // url: 'https://todaynowork.group/we_chat-0.0.1-SNAPSHOT/coursejson',
+  //     header: { "Content-Type": "application/json" },
+  //     success: function (res) {
+  //       var data = res.data;
+  //       console.log('data = ' + JSON.stringify(data));
+  //       var temp = [];
+  //       // temp.push({ 'name': '今日热闻', 'thumbnail': null, 'description': '今日热闻' })
+  //       for (var i in data.others) {
+  //         temp.push(data.others[i]);
+  //       }
+  //       that.setData({ themes: temp });
+  //     },
+  //     fail: function (error) {
+  //       console.log(error)
+  //     }
+  //   });
+  // },
 
 })
