20f0f3d7db971f8b867d1c75d5d5839ae2f9200c
DUO ZHANG <duozhang@cn.ibm.com>
2017-11-21 13:51:01 +0800

    update label pop up

--- pages/label/label.js
+++ pages/label/label.js
@@ -8,37 +8,23 @@ Page({
       { name: '../../image/computer_uncheck@1x.png', value: '服务器' },
       { name: '../../image/agile_uncheck@1x.png', value: 'Agile' }
     ],
-    radioItems: [
-      {name: 'USA', value: '美国'},
-      {name: 'CHN', value: '中国'}
-    ],
-    hidden: false
+    hiddenModal: true
   },
-  checkboxChange: function (e) {
-    var checked = e.detail.value
-    var changed = {}
-    for (var i = 0; i < this.data.checkboxItems.length; i++) {
-      if (checked.indexOf(this.data.checkboxItems[i].name) !== -1) {
-        changed['checkboxItems[' + i + '].checked'] = true
-      } else {
-        changed['checkboxItems[' + i + '].checked'] = false
-      }
-    }
-    this.setData(changed)
+  listenerButton: function () {
+    this.setData({
+      hiddenModal: !this.data.hiddenModal
+    })
   },
-  radioChange: function (e) {
-    var checked = e.detail.value
-    var changed = {}
-    for (var i = 0; i < this.data.radioItems.length; i ++) {
-      if (checked.indexOf(this.data.radioItems[i].name) !== -1) {
-        changed['radioItems[' + i + '].checked'] = true
-      } else {
-        changed['radioItems[' + i + '].checked'] = false
-      }
-    }
-    this.setData(changed)
+
+  listenerConfirm: function () {
+    this.setData({
+      hiddenModal: true
+    })
   },
-  tapEvent: function (e) {
-    console.log('按钮被点击')
-  }
+
+  listenerCancel: function () {
+    this.setData({
+      hiddenModal: true
+    })
+  },
 })
